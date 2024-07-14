614798ab423647b468f6425dca7a57d0671594a4
Gothax <scott990328@gmail.com>
2024-07-14 19:12:53 +0900

    init

---
+++ worldcup/src/main/java/ideal/worldcup/WorldcupApplication.java
@@ -0,0 +1,13 @@
+package ideal.worldcup;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorldcupApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WorldcupApplication.class, args);
+	}
+
+}
---
+++ worldcup/src/test/java/ideal/worldcup/WorldcupApplicationTests.java
@@ -0,0 +1,13 @@
+package ideal.worldcup;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorldcupApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
