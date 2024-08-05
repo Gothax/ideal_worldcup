41c8d3e18477a654adfe8d5238c20e1d57b92ceb
Gothax <scott990328@gmail.com>
2024-08-05 17:15:17 +0900

    init

---
+++ ideal-worldcup-reboot/src/main/java/com/gothaxcity/idealworldcupreboot/IdealWorldcupRebootApplication.java
@@ -0,0 +1,13 @@
+package com.gothaxcity.idealworldcupreboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IdealWorldcupRebootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IdealWorldcupRebootApplication.class, args);
+	}
+
+}
---
+++ ideal-worldcup-reboot/src/test/java/com/gothaxcity/idealworldcupreboot/IdealWorldcupRebootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gothaxcity.idealworldcupreboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IdealWorldcupRebootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
