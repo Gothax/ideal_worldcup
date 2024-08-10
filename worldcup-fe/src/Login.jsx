e5837e7ccc763ad98719d68c5fdc99020279fe66
Gothax <scott990328@gmail.com>
2024-08-10 21:10:36 +0900

    로그인 로직 수정 / still 403

--- worldcup-fe/src/Login.jsx
+++ worldcup-fe/src/Login.jsx
@@ -1,19 +1,32 @@
 import {GoogleLogin} from "@react-oauth/google";
 import {GoogleOAuthProvider} from "@react-oauth/google";
+import axios from "axios";
 
 const GoogleLoginButton = () => {
     const clientId = "405722211943-pos46fpdksbapn9ts2cef64qs3v494ru.apps.googleusercontent.com"
+
+    const handleLoginSuccess = async (res) => {
+        console.log(res);
+        const idToken = res.credential;
+
+        try{
+            const response = await axios.get("http://localhost:8080/auth/success", {idToken});
+            console.log(response);
+        } catch (error) {
+            console.log(error);
+        }
+    }
+
+    const handleLoginFailure = (err) => {
+        console.log(err);
+    }
 
     return (
         <>
             <GoogleOAuthProvider clientId={clientId}>
                 <GoogleLogin
-                    onSuccess={(res) => {
-                        console.log(res);
-                    }}
-                    onFailure={(err) => {
-                        console.log(err);
-                    }}
+                    onSuccess={handleLoginSuccess}
+                    onFailure={handleLoginFailure}
                 />
             </GoogleOAuthProvider>
         </>
